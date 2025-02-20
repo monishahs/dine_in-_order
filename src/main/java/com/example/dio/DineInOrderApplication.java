d87d271ef069d09711cd62fec45164d044f4d820
Monisha <monishahs42@gmail.com>
2025-02-20 09:14:18 +0530

    adding basic file

---
+++ src/main/java/com/example/dio/DineInOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineInOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineInOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dio/DineInOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineInOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
